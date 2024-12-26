90b976ed56fe9bd84948eb123f64f2745c1ffcbb
MissSecrecy <Secrecy4444@gmx.de>
2024-12-26 22:00:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-fetch("data/cards.json")
+fetch("https://github.com/MissSecrecy/Kartenspiel/blob/main/data/cards.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];
